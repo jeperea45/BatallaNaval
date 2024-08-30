1beeaa3406ecf0928c5e2796acae5c95c984bc68
Juan <jeperea45@gmail.com>
2024-08-30 17:24:51 -0500

    Primera fase de batalla naval

---
+++ batallaNaval1.py
@@ -0,0 +1,24 @@
+import random
+
+def crearTablero(dimension):
+    return [["~" for _ in range(dimension)] for _ in range(dimension)]
+
+def mostrartTableros(tableroDisparoJugador, tableroDisparosOponentes):
+    print("\n Tablero de disparos: ")
+    for fila in tableroDisparoJugador:
+        print(" ".join(fila))
+    print("\n Tablero de disparos del oponente: ")
+    for fila in tableroDisparosOponentes:
+        print(" ".join(fila))
+
+def colocarBarcos(tablero, barcos, jugador):
+    for barco in barcos:
+        colocado: False
+        while not colocado:
+            if jugador == "jugador":
+                print(f"Colocado {barco ['nombre']} de tamaño {barco['dimension']}")
+                fila = int(input("Ingrese la fila: "))
+                columna = int(input("Ingrese la columna: "))
+                orientacion = int(input("Ingrese la orientación (h para horizontal, v para vertical): ")).lower()
+                
+    
